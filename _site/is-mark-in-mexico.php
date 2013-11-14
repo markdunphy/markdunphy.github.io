d237292d2af60396a4a938db57d609713df3c7f8
Mark Dunphy <markdunphy@gmail.com>
2013-11-14 18:46:56 -0500

    add is mark in mexico page

---
+++ _site/is-mark-in-mexico.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title></title>
+	<style>
+	h1 {
+		font-family: Helvetica, Arial, sans-serif;
+		text-align: center;
+		font-size: 3em;
+		width: 100%;
+		height: 56px;
+		background-color: green;
+		margin-top: -28px;
+		position: absolute;
+		top: 50%;
+	}
+	</style>
+</head>
+<body>
+	<h1>Yes.</h1>
+</body>
+</html>
